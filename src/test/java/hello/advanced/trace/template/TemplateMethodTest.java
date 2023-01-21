1206cf850a8031dace86a59fc9d50d6a9b6f7c83
nohsion <shtldhs123@naver.com>
2023-01-21 21:08:55 +0900

    템플릿 메서드 패턴 - 익명 내부 클래스 사용

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -30,6 +30,30 @@ class TemplateMethodTest {
         template2.execute();
     }
 
+    /**
+     * 템플릿 메서드 패턴, 익명 내부 클래스 사용
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("AbstractTemplate 클래스 이름1={}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("AbstractTemplate 클래스 이름2={}", template2.getClass());
+        template2.execute();
+    }
+
     private void logic1() {
         long startTime = System.currentTimeMillis();
         // 비즈니스 로직 실행
