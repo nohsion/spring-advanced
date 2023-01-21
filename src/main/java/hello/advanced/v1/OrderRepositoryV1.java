a5d9cc60889f647801088f5407fe2759ac9c3031
nohsion <shtldhs123@naver.com>
2023-01-21 18:29:28 +0900

    로그추적기 V1

---
+++ src/main/java/hello/advanced/v1/OrderRepositoryV1.java
@@ -0,0 +1,42 @@
+package hello.advanced.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    private static final String EXCEPTION_ITEM = "ex";
+
+    public void save(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+            // 저장 로직
+            if (itemId.equals(EXCEPTION_ITEM)) {
+                throw new IllegalStateException("예외 발생");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
